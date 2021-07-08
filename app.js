50f9f669c5544f02740bec456229b515028f7985
Reid Braswell <reidbras@gmail.com>
2021-07-08 18:16:07 -0500

    Added code for exercise

--- app.js
+++ app.js
@@ -2,3 +2,13 @@ console.log("Hello World!\n==========\n");
 
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
+
+function plus(num) {
+    return function(plusNum) {
+        return num + plusNum;
+    }
+}
+
+const plus15 = plus(15);
+console.log(plus15(20));
+
