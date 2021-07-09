c5e0f57605d991fda48269f22a268595f1e82dd1
Reid Braswell <reidbras@gmail.com>
2021-07-09 15:40:59 -0500

    Adding form validation and control function

--- app.js
+++ app.js
@@ -12,3 +12,32 @@ function plus(num) {
 const plus15 = plus(15);
 console.log(plus15(20));
 
+/*
+ * Controlling function that calls other
+ * functions:
+ *
+ * 1. arraySum()
+ * 2. bookProcessing()
+ * 3. Updates console.log and screen
+ * 4. Reset the form
+ *
+ */
+function controller(arrayP, bookTitleP, numberOfPagesP, numberTimesReadP) {
+  console.log("Begin controller()");
+  console.log(
+    `Parameters = Array:"${arrayP}", BookTitle:"${bookTitleP}", NumberOfPages:"${numberOfPagesP}", NumberTimesRead:"${numberTimesReadP}"`
+  );
+
+ 
+  }
+
+/*
+ * Form validation function.
+ *
+ */
+function validateForm() {
+    console.log("Begin validateForm()");
+    return true;
+  }
+  
+
